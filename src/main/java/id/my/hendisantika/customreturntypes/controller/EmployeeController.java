d2458d865d38f3da5384b837a0cc21cfe1a63690
hendisantika <hendisantika@yahoo.co.id>
2025-03-02 06:07:04 +0700

    feat: Add getAllEmployees in EmployeeController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/customreturntypes/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/customreturntypes/controller/EmployeeController.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.customreturntypes.controller;
 
+import id.my.hendisantika.customreturntypes.entity.Employee;
 import id.my.hendisantika.customreturntypes.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+
+    // Get all employees
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
 }
