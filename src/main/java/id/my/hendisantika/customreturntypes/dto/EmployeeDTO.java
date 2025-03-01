72b4f07f744cf5094ab3a05cab3d2052bfdc7426
hendisantika <hendisantika@yahoo.co.id>
2025-03-02 06:02:16 +0700

    feat: Add Employee DTO🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/customreturntypes/dto/EmployeeDTO.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.customreturntypes.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jpa-custom-return-types
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 02/03/25
+ * Time: 06.01
+ * To change this template use File | Settings | File Templates.
+ */
+public record EmployeeDTO(String name, String email) {
+}
