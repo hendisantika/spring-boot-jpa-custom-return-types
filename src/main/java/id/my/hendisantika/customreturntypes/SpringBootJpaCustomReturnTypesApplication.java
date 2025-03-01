6c011d1f6eb016d48c6428c1c77c7e608bee7dd0
hendisantika <hendisantika@yahoo.co.id>
2025-03-02 05:57:50 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/customreturntypes/SpringBootJpaCustomReturnTypesApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.customreturntypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaCustomReturnTypesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaCustomReturnTypesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/customreturntypes/SpringBootJpaCustomReturnTypesApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.customreturntypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaCustomReturnTypesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
