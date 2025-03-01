20a0f77a3a885b1c9b3134ec16b528d019a8aa3b
hendisantika <hendisantika@yahoo.co.id>
2025-03-02 06:02:58 +0700

    feat: Add EmployeeProjection🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/customreturntypes/projection/EmployeeProjection.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.customreturntypes.projection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jpa-custom-return-types
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 02/03/25
+ * Time: 06.02
+ * To change this template use File | Settings | File Templates.
+ */
+public interface EmployeeProjection {
+    String getName();
+
+    String getEmail();
+}
