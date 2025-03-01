ea949371e8090299fb2e0c57e0296f6e7a4aad57
hendisantika <hendisantika@yahoo.co.id>
2025-03-02 06:04:48 +0700

    feat: Add EmployeeService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/customreturntypes/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.customreturntypes.service;
+
+import id.my.hendisantika.customreturntypes.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jpa-custom-return-types
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 02/03/25
+ * Time: 06.04
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+}
