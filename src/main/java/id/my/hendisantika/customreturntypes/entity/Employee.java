d19f398783e933a37939aa415e7b6eb73b71b4df
hendisantika <hendisantika@yahoo.co.id>
2025-03-02 06:01:44 +0700

    feat: Add Employee Entity🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/customreturntypes/entity/Employee.java
@@ -0,0 +1,37 @@
+package id.my.hendisantika.customreturntypes.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jpa-custom-return-types
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 02/03/25
+ * Time: 06.00
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String department;
+}
